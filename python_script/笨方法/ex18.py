0681836fb7dfa8017d3cbe35fe8c298e8f808a15
chenzizhou <nature_chen2021@163.com>
2022-10-09 16:27:19 +0800

    更新

--- python_script/笨方法/ex18.py
+++
@@ -1,7 +0,0 @@
-# coding=utf-8
-# 函数接收多个参数*args
-def function(*args):
-    # 将接收到的参数赋给变量
-    username,password=args
-    print(username,password)
-function('nature',123456)
--- python_script/笨方法/ex24.py
+++
@@ -1,4 +0,0 @@
-word="""you must be tring hard to studing,because you are a man and 
-shoud make much monney to let people around you live a good life，
-so keep your origin heart doing any thing"""
-print(word)
