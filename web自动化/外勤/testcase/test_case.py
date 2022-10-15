293b1e664a8cfabf9390a8461edb1aa634cfdb93
chenzizhou <nature_chen2021@163.com>
2022-10-15 18:00:56 +0800

    gx

--- web自动化/外勤/testcase/test_case.py
+++ web自动化/外勤/testcase/test_case.py
@@ -3,7 +3,7 @@
 # 开发时间：2022/10/11 22:30
 # 功能：
 import unittest
-from ddt import ddt, data
+from ddt import ddt, data, unpack
 from selenium import webdriver
 from time import sleep
 from web自动化.外勤.pageobject.login_page import LoginPage
@@ -11,9 +11,10 @@ from web自动化.外勤.pageobject.login_page import LoginPage
 @ddt
 class TestCase(unittest.TestCase):
     @data(('admin', '123456'), ('nature', '123456'))
-    def test_01_login(self,args):
+    @unpack
+    def test_01_login(self,username,password):
         l = LoginPage()
-        l.login(*args)
+        l.login(username,password)
         self.assertTrue(l.get_expect_element())
         l.close()
 
