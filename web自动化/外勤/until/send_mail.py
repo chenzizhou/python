7800fc2ee8441f89585b12b989d49448a471e335
chenzizhou <nature_chen2021@163.com>
2022-10-17 23:02:25 +0800

    更新

--- web自动化/外勤/until/send_mail.py
+++ web自动化/外勤/until/send_mail.py
@@ -7,6 +7,7 @@
 #发送邮箱服务器
 import smtplib
 from email.header import Header
+from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 
@@ -22,15 +23,26 @@ def send_mail(file_new):
     #发送邮箱
     sender = '1096059759@qq.com'
     #接收邮箱
-    receiver = '1096059759@qq.com'
+    # receiver = 'nature_chen2021@163.com'
+    receiver = '417753633@qq.com'
     #发送邮件主题
     subject= '外勤登录'
+
     #编写HTML类型的邮件正文
-    msg = MIMEText(mail_body,'html','utf-8 ')
-    msg['Subject'] = Header (subject,'utf-8 ')
+    # msg = MIMEText(mail_body, 'html', 'utf-8')
+    # msg['Subject'] = Header(subject,'utf-8')
+
+    # 附件
+    att = MIMEText(mail_body, 'html', 'utf-8')
+    att["Content-Type"] = 'application/octet-stream'
+    att["content-Disposition"] = 'attachment; filename='+file_new
+    msgRoot = MIMEMultipart('related')
+    msgRoot.attach(att)
+    msgRoot['Subject'] = Header(subject, 'utf-8')
+
     #连接发送邮件
     smtp =smtplib.SMTP()
     smtp.connect(smtpserver)
-    smtp.login(user, password)
-    smtp.sendmail(sender,receiver,msg.as_string())
+    smtp.login(user,password)
+    smtp.sendmail(sender,receiver,msgRoot.as_string())
     smtp.quit()
