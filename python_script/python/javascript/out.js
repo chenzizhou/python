4c67b736239629fcf4c78229bb2037e9427e06aa
chenzizhou <nature_chen2021@163.com>
2022-10-28 10:26:13 +0800

    更新

---
+++ python_script/python/javascript/out.js
@@ -0,0 +1,6 @@
+function add(a,b){
+    var c;
+    c=a+b;
+    document.write(c);
+    console.log(c);
+}
---
+++ web自动化/test/标准输出.py
@@ -0,0 +1,8 @@
+# 作者
+# NATURE
+# 日期
+# 2022/10/27 9:59
+# 功能
+#
+f=open('标准输出.txt','wt')
+print('hello world!',file=f)
