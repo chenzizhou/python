919623a54f58596fa52ba35c5d45766187eacffa
chenzizhou <nature_chen2021@163.com>
2022-11-06 11:11:11 +0800

    更新

--- python_script/python/python基础/内置函数.py
+++ python_script/python/python基础/内置函数.py
@@ -4,10 +4,17 @@
 # 2022/10/11 10:56
 # 功能
 #
+'''人'''
 class Person():
     def method(self):
+        '''打印'''
         print('abc')
 p=Person()
+setattr(p, 'name','nature')
+print(p.name)
 m=getattr(p,'method')
 m()
+print(dir(p))
+
+print(Person.method.__doc__)
 
