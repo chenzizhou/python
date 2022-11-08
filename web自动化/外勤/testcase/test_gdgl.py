c91c0e5913d6827803a1944a969284c612bf5917
chenzizhou <nature_chen2021@163.com>
2022-11-08 22:51:02 +0800

    gx

--- web自动化/外勤/testcase/test_gdgl.py
+++ web自动化/外勤/testcase/test_gdgl.py
@@ -6,15 +6,16 @@ from web自动化.外勤.pageobject.gd_page import WxgdPage
 class TestGdgl(unittest.TestCase):
     def test_wxgd(self):
         try:
-            username='bhcs'
-            password='123456'
-            g=WxgdPage()
-            g.save_wxsb(username,password)
+            username = 'bhcs'
+            password = '123456'
+            g = WxgdPage()
+            g.save_wxsb(username, password)
         except Exception as e:
             print(e)
         finally:
             pass
             # g.close()
+
 
 if __name__ == '__main__':
     unittest.main()
