9de27b19b16f66637e6a8da2175d0df1951090bf
chenzizhou <nature_chen2021@163.com>
2022-11-13 20:57:06 +0800

    gx

--- python_script/python/面向对象编程/类.py
+++ python_script/python/面向对象编程/类.py
@@ -1,17 +1,32 @@
-class Student:
-    pass
+class Student(object):
+    '''学生类'''
+
+    def s_m(self):
+        print('学生methond')
+        return '动态方法调用'
+
 
 def out_print(lst):
     for i in lst:
-        print(i.name,i.age)
-l=[]
+        print(i.name, i.age)
+        print(getattr(i, 's_m')())
+        print(i.__doc__)
+        print(i.__dict__)
+        print(i.__class__)
+        print(i.__class__.__bases__)
+        print(i.__class__.__base__)
+        print(isinstance(i, Student))
+        print(type(i))
+
+
+l = []
 while True:
-    name=input('姓名:')
+    name = input('姓名:')
     if not name:
         break
-    age=input('年龄:')
-    stu=Student()
-    stu.name=name
-    stu.age=age
+    age = input('年龄:')
+    stu = Student()
+    stu.name = name
+    stu.age = age
     l.append(stu)
-out_print(l)        
+out_print(l)
