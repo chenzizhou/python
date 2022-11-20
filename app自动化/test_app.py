a5fc09e3f1f9fa7dba41232c7447595e6a45618a
chenzizhou <nature_chen2021@163.com>
2022-11-21 02:56:14 +0800

    gx

--- app自动化/test_app.py
+++ app自动化/test_app.py
@@ -4,17 +4,23 @@
 # 功能：
 from appium import webdriver
 from appium.webdriver.extensions.android.nativekey import AndroidKey
+from selenium.webdriver.common.by import By
 
 desired_caps = {
     'platformName': 'Android',  # 被测手机是安卓
-    'platformVersion': '8',#手机安卓版本
-    'deviceName': 'xxx',  # 设备名，安卓手机可以随意填写
-    'appPackage': 'tv.danmaku.bili',  # 启动APP Package名称
-    'appActivity': '.ui.splash.SplashActivity ',  # 启动Activity名称#
+    'platformVersion': '12',  # 手机安卓版本
+    'deviceName': 'mix fold',  # 设备名，安卓手机可以随意填写
+    'appPackage': 'com.alibaba.android.rimet',  # 启动APP Package名称
+    'appActivity': '.biz.LaunchHomeActivity',  # 启动Activity名称#
     'unicodeKeyboard ': True,  # 使用自带输入法，输入中文时填True
     'resetKeyboard': True,  # 执行完程序恢复原来输入法
     'noReset': True,  # 不要重置App
     'newCommandTimeout': 6000,
-    'automationName': 'UiAutomator2'
+    'automationName': 'UiAutomator2',
+    'ignoreHiddenApiPolicyError': True
     # 'app' : r'd:\apk\bili.apk'
 }
+driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+driver.implicitly_wait(10)
+driver.find_elements(By.ID, 'home_app_item')[2].click()
+driver.find_element(By.XPATH, '//android.view.View[text="考勤打卡"]').click()
