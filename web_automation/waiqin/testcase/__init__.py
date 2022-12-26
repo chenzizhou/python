bdd412ef262e8ec60563a8697f1dcee38714a402
chenzizhou <nature_chen2021@163.com>
2022-12-26 17:04:03 +0800

    更新

--- web_automation/waiqin/testcase/__init__.py
+++ web_automation/waiqin/testcase/__init__.py
@@ -2,3 +2,4 @@
 # 作者：NATURE
 # 开发时间：2022/10/11 22:29
 # 功能：
+import web_automation.waiqin.pageobject
---
+++ web_automation/waiqin/testcase/test_import.py
@@ -0,0 +1,9 @@
+# 作者
+# NATURE
+# 日期
+# 2022/12/23 17:13
+# 功能
+#
+from web_automation.waiqin.pageobject.test_import import print_123
+
+print_123()
