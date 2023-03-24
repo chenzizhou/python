63bc8eb6a78ea02042d47b42995755de33dcc572
chenzizhou <nature_chen2021@163.com>
2023-03-24 15:51:35 +0800

    gx

--- interface_automation/testcase/wq_ywpz.py
+++ interface_automation/testcase/wq_ywpz.py
@@ -7,9 +7,15 @@
 import requests
 import yaml
 
-rep=requests.session()
-url='http://192.168.10.195:7799/patrolServer/bp/configure/api/manager/schemeType/add?' \
-    'access_token=fbc1aebc6907d0c15c9cdbc6a9673a735a39caa9717a04f56e213dff7102a492f03e5bfa33ece9c85cd8de7057ec4f1129970b609a6a2d90642a9fb19c79fe4404a9ae29396e9712'
+req = requests.session()
+url = 'http://10.41.16.20:32091/hbp/userService/login?&type=1&sys=PATROL&sysVersion=&operSys=Windows&client=Chrome%3A' \
+      '%20113.0.0.0&platformType=pc&userAndPwd=e%2BUBvF1KiOOdARU4x67e6b' \
+      '%2Fcz7UoUM3HOWFKofhBhA3t82IdiMJWah9tcdfPdiNzHXCvyTHg2LS4XQskcnwKJPqKhhqveKcM3UVePDANAyE9zuI84EgZFnN6xTiySrwhoUC2hm73hXbxLoRvIDWXesvvIGq%2B5VjPGzPZtAEZwtA%3D&access_token= '
+rep=req.request('get', url=url)
+print(rep.json())
+token=rep.json()['token']
+print(token)
+url='http://10.41.16.20:32091/patrolServer/bp/configure/api/manager/schemeType/add?access_token='+token
 data={
     "arriveRate": 1,
     "coverRate": 1,
@@ -221,22 +227,42 @@ data={
             "sort": 4
         }
     ],
-    "appAdvanceBtn": "[{\"taskName\":\"转单\",\"taskId\":\"BUTTON_ID_TRANSFORM\",\"needPause\":False,\"sort\":1},{\"taskName\":\"退单\",\"taskId\":\"BUTTON_ID_BACK\",\"needPause\":False,\"sort\":2},{\"taskName\":\"延期\",\"taskId\":\"BUTTON_ID_DELAY\",\"needPause\":False,\"sort\":3},{\"taskName\":\"备注\",\"taskId\":\"BUTTON_ID_REMARK\",\"needPause\":False,\"sort\":4}]",
-    "appFinishBtn": "[{\"taskName\":\"上报\",\"taskId\":\"BUTTON_ID_EVENT_REPORT\",\"needPause\":False,\"sort\":2},{\"taskName\":\"备注\",\"taskId\":\"BUTTON_ID_REMARK\",\"needPause\":False,\"sort\":3}]",
-    "appGroupBtn": "[{\"taskName\":\"反馈\",\"taskId\":\"BUTTON_ID_FEED_BACK\",\"needPause\":False,\"sort\":1},{\"taskName\":\"上报\",\"taskId\":\"BUTTON_ID_EVENT_REPORT\",\"needPause\":False,\"sort\":1}]",
-    "appOverBtn": "[{\"taskName\":\"上报\",\"taskId\":\"BUTTON_ID_EVENT_REPORT\",\"needPause\":False,\"sort\":2},{\"taskName\":\"备注\",\"taskId\":\"BUTTON_ID_REMARK\",\"needPause\":False,\"sort\":3}]",
-    "appWaitdoBtn": "[{\"taskName\":\"上报\",\"taskId\":\"BUTTON_ID_EVENT_REPORT\",\"needPause\":False,\"sort\":2},{\"taskName\":\"完工\",\"taskId\":\"BUTTON_ID_FINISH\",\"needPause\":False,\"sort\":3},{\"taskName\":\"转单\",\"taskId\":\"BUTTON_ID_TRANSFORM\",\"needPause\":False,\"sort\":4},{\"taskName\":\"退单\",\"taskId\":\"BUTTON_ID_BACK\",\"needPause\":False,\"sort\":5},{\"taskName\":\"延期\",\"taskId\":\"BUTTON_ID_DELAY\",\"needPause\":False,\"sort\":6},{\"taskName\":\"备注\",\"taskId\":\"BUTTON_ID_REMARK\",\"needPause\":False,\"sort\":7}]",
-    "appMapEquipBtn": "[{\"taskName\":\"反馈\",\"taskId\":\"BUTTON_ID_FEED_BACK\",\"needPause\":False,\"sort\":1},{\"taskName\":\"上报\",\"taskId\":\"BUTTON_ID_EVENT_REPORT\",\"needPause\":False,\"sort\":3},{\"taskName\":\"历史记录\",\"taskId\":\"BUTTON_ID_HISTORY\",\"needPause\":False,\"sort\":2},{\"taskName\":\"导航\",\"taskId\":\"BUTTON_ID_NAVIGATION\",\"needPause\":False,\"sort\":4}]",
-    "appEquipBtn": "[{\"taskName\":\"反馈\",\"taskId\":\"BUTTON_ID_FEED_BACK\",\"needPause\":False,\"sort\":1},{\"taskName\":\"上报\",\"taskId\":\"BUTTON_ID_EVENT_REPORT\",\"needPause\":False,\"sort\":2},{\"taskName\":\"导航\",\"taskId\":\"BUTTON_ID_NAVIGATION\",\"needPause\":False,\"sort\":3},{\"taskName\":\"别名\",\"taskId\":\"BUTTON_ID_ALIAS\",\"needPause\":False,\"sort\":4},{\"taskName\":\"历史记录\",\"taskId\":\"BUTTON_ID_HISTORY\",\"needPause\":False,\"sort\":5}]",
-    "appMapBtn": "[{\"taskName\":\"上报\",\"taskId\":\"BUTTON_ID_EVENT_REPORT\",\"needPause\":False,\"sort\":1},{\"taskName\":\"工具\",\"taskId\":\"BUTTON_ID_MAP_TOOL\",\"needPause\":False,\"sort\":2},{\"taskName\":\"实时\",\"taskId\":\"BUTTON_ID_REAL_LOCATION\",\"needPause\":False,\"sort\":3},{\"taskName\":\"轨迹\",\"taskId\":\"BUTTON_ID_TRACE\",\"needPause\":False,\"sort\":4}]",
-    "type": "YH",
-    "code": "12345",
-    "name": "12345",
+    "appAdvawnceBtn": "[{\"taskName\":\"转单\",\"taskId\":\"BUTTON_ID_TRANSFORM\",\"needPause\":False,\"sort\":1},"
+                      "{\"taskName\":\"退单\",\"taskId\":\"BUTTON_ID_BACK\",\"needPause\":False,\"sort\":2},"
+                      "{\"taskName\":\"延期\",\"taskId\":\"BUTTON_ID_DELAY\",\"needPause\":False,\"sort\":3},"
+                      "{\"taskName\":\"备注\",\"taskId\":\"BUTTON_ID_REMARK\",\"needPause\":False,\"sort\":4}]",
+    "appFinishBtn": "[{\"taskName\":\"上报\",\"taskId\":\"BUTTON_ID_EVENT_REPORT\",\"needPause\":False,\"sort\":2},"
+                    "{\"taskName\":\"备注\",\"taskId\":\"BUTTON_ID_REMARK\",\"needPause\":False,\"sort\":3}]",
+    "appGroupBtn": "[{\"taskName\":\"反馈\",\"taskId\":\"BUTTON_ID_FEED_BACK\",\"needPause\":False,\"sort\":1},"
+                   "{\"taskName\":\"上报\",\"taskId\":\"BUTTON_ID_EVENT_REPORT\",\"needPause\":False,\"sort\":1}]",
+    "appOverBtn": "[{\"taskName\":\"上报\",\"taskId\":\"BUTTON_ID_EVENT_REPORT\",\"needPause\":False,\"sort\":2},"
+                  "{\"taskName\":\"备注\",\"taskId\":\"BUTTON_ID_REMARK\",\"needPause\":False,\"sort\":3}]",
+    "appWaitdoBtn": "[{\"taskName\":\"上报\",\"taskId\":\"BUTTON_ID_EVENT_REPORT\",\"needPause\":False,\"sort\":2},"
+                    "{\"taskName\":\"完工\",\"taskId\":\"BUTTON_ID_FINISH\",\"needPause\":False,\"sort\":3},"
+                    "{\"taskName\":\"转单\",\"taskId\":\"BUTTON_ID_TRANSFORM\",\"needPause\":False,\"sort\":4},"
+                    "{\"taskName\":\"退单\",\"taskId\":\"BUTTON_ID_BACK\",\"needPause\":False,\"sort\":5},"
+                    "{\"taskName\":\"延期\",\"taskId\":\"BUTTON_ID_DELAY\",\"needPause\":False,\"sort\":6},"
+                    "{\"taskName\":\"备注\",\"taskId\":\"BUTTON_ID_REMARK\",\"needPause\":False,\"sort\":7}]",
+    "appMapEquipBtn": "[{\"taskName\":\"反馈\",\"taskId\":\"BUTTON_ID_FEED_BACK\",\"needPause\":False,\"sort\":1},"
+                      "{\"taskName\":\"上报\",\"taskId\":\"BUTTON_ID_EVENT_REPORT\",\"needPause\":False,\"sort\":3},"
+                      "{\"taskName\":\"历史记录\",\"taskId\":\"BUTTON_ID_HISTORY\",\"needPause\":False,\"sort\":2},"
+                      "{\"taskName\":\"导航\",\"taskId\":\"BUTTON_ID_NAVIGATION\",\"needPause\":False,\"sort\":4}]",
+    "appEquipBtn": "[{\"taskName\":\"反馈\",\"taskId\":\"BUTTON_ID_FEED_BACK\",\"needPause\":False,\"sort\":1},"
+                   "{\"taskName\":\"上报\",\"taskId\":\"BUTTON_ID_EVENT_REPORT\",\"needPause\":False,\"sort\":2},"
+                   "{\"taskName\":\"导航\",\"taskId\":\"BUTTON_ID_NAVIGATION\",\"needPause\":False,\"sort\":3},"
+                   "{\"taskName\":\"别名\",\"taskId\":\"BUTTON_ID_ALIAS\",\"needPause\":False,\"sort\":4},"
+                   "{\"taskName\":\"历史记录\",\"taskId\":\"BUTTON_ID_HISTORY\",\"needPause\":False,\"sort\":5}]",
+    "appMapBtn": "[{\"taskName\":\"上报\",\"taskId\":\"BUTTON_ID_EVENT_REPORT\",\"needPause\":False,\"sort\":1},"
+                 "{\"taskName\":\"工具\",\"taskId\":\"BUTTON_ID_MAP_TOOL\",\"needPause\":False,\"sort\":2},"
+                 "{\"taskName\":\"实时\",\"taskId\":\"BUTTON_ID_REAL_LOCATION\",\"needPause\":False,\"sort\":3},"
+                 "{\"taskName\":\"轨迹\",\"taskId\":\"BUTTON_ID_TRACE\",\"needPause\":False,\"sort\":4}]",
+    "type": "XJ",
+    "code": "321",
+    "name": "321",
     "jobRegiod": "[]",
     "warnConfig": "[]"
 }
-req=rep.post(url,json=data)
-rep.get('http://192.168.10.195:7799/patrolServer/proxy/svr/ServiceEngine/rest/patrolService/refreshConfigCache?access_token=fbc1aebc6907d0c15c9cdbc6a9673a735a39caa9717a04f56e213dff7102a492f03e5bfa33ece9c85cd8de7057ec4f1129970b609a6a2d90642a9fb19c79fe4404a9ae29396e9712')
-with open('ywpz.yaml','wt',encoding='utf8') as f:
-    yaml.dump(req.json(),f,allow_unicode=True,encoding='utf8')
-print(req.json())
+heads={}
+print(req.request(method='post',url=url,data=data).json())
+print(req.request(method='post',url=url,data=data).json()['data'])
+
