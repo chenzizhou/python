b5a480c20145e18781980eb4e10e262581458a43
陈自州 <95033787+chenzizhou@users.noreply.github.com>
2025-05-29 16:58:23 +0800

    Update register.py

--- flask_test/blueprints/register.py
+++ flask_test/blueprints/register.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, redirect, url_for
 from flask_wtf import FlaskForm
 from werkzeug.security import generate_password_hash
 from wtforms import ValidationError, StringField, PasswordField, SubmitField, DateField, DateTimeField
-from wtforms.validators import InputRequired, Email
+from wtforms.validators import InputRequired
 from exts import db
 from models import UserModel
 
@@ -26,8 +26,10 @@ def check_login(form, field):
 class Register(FlaskForm):
     username = StringField(label='用户名', validators=[InputRequired("用户名不能为空"), check_login],
                            render_kw={'placeholder': '输入用户名'})
-    password = PasswordField(label='密　码', validators=[InputRequired("密码不能为空"), check_login],render_kw={'placeholder': '输入密码'})
-    email = StringField(label='邮 箱', validators=[InputRequired("邮箱不能为空"), Email('邮箱格式错误')],render_kw={'placeholder': '输入邮箱'})
+    password = PasswordField(label='密　码', validators=[InputRequired("密码不能为空"), check_login],
+                             render_kw={'placeholder': '输入密码'})
+    email = StringField(label='邮 箱', validators=[InputRequired("邮箱不能为空")],
+                        render_kw={'placeholder': '输入邮箱'})  # , Email('邮箱格式错误')
     # join_time = DateTimeField(label="添加时间", default=datetime.now())
     submit = SubmitField(label="提交")
 
@@ -42,7 +44,8 @@ def register_page():
     if form.validate_on_submit():
         print([i for i in request.form.values()])
         print('添加用户')
-        user1 = UserModel(username=form.username.data, password=generate_password_hash(form.password.data), email=form.email.data)
+        user1 = UserModel(username=form.username.data, password=generate_password_hash(form.password.data),
+                          email=form.email.data)
         # 增加用户到数据库
         db.session.add(user1)
         db.session.commit()
